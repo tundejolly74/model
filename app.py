9881e686a1913e1f33a53d2a31a9782d00b31a29
tundejolly74 <97757362+tundejolly74@users.noreply.github.com>
2024-09-26 02:56:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ def predict():
     return jsonify({'predicted_sales': '...'})  # Replace with your logic
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
